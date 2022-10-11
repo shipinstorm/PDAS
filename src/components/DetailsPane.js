6fd7f2c607626439aa641cd1af59ddd4ee2f6c73
tinydragon715 <vladimir715.han@gmail.com>
2022-10-11 12:47:25 +0200

    fix: merge yarn and npm to yarn

--- src/components/DetailsPane.js
+++ src/components/DetailsPane.js
@@ -1,4 +1,6 @@
-import { useSelector } from "react-redux";
+import { useState } from 'react';
+
+import '../assets/css/DetailsPane.scss';
 
 function DetailsPane({
   isHidden,
@@ -6,33 +8,103 @@ function DetailsPane({
 }) {
   let className = "details-pane";
   className += isHidden ? " hidden" : "";
+
+  const [showMetaData, setShowMetaData] = useState(false);
   
-  const graphData = useSelector((state) => state.global.graphData);
-	const arrayData = useSelector((state) => state.global.arrayData);
-	const taskData = useSelector((state) => state.global.taskData);
-  const jobIDs = jobSelected.split('.');
-  const graphID = jobIDs[0];
-  const arrayID = jobIDs[1];
-  const taskID = jobIDs[2];
-
-  // console.log(graphData);
-  // console.log(arrayData[graphID][0]);
-  // console.log(taskData[graphID][arrayID]);
-
   return (
     <div className={className}>
-      Select job to view details<br />
-      <p>Job ID: <span>{jobSelected}</span></p>
-      <p>sleep 100</p>
-      <p>gwhitted</p>
-      <p>5:44 pm</p>
-      <p>Frames</p>
-      <p>No available frames</p>
-      <p>Status</p>
-      <p>Memory (MB)</p>
-      <p>Elapsed Time</p>
-      <p>Priority</p>
-      <p>Show All Metadata</p>
+      <div className='Details'>
+        {/* <span>Select job to view details</span> */}
+        <p className='Details-No-Margin'>
+          Job ID:&nbsp;
+          <span className='Details-Light'>{jobSelected}</span>
+        </p>
+        <p className='Details-Title'>sleep 100</p>
+        <div className='Details-Row'>
+          <p className='Details-Header'>gwhitted</p>
+          <p className='Details-Right'>5:44 pm</p>
+        </div>
+        <div className='Details-Row'>
+          <p className='Details-Header'>Frames</p>
+          <p className='Details-Dark'>No available frames</p>
+        </div>
+        <div className='Details-Row'>
+          <p className='Details-Header'>Status</p>
+        </div>
+        <div className='Details-Row'>
+          <p className='Details-Header'>
+            Memory&nbsp;
+            <span className='Details-Dark'>(MB)</span>
+          </p>
+          <p className='Details-Light'>
+            500&nbsp;
+            <span className='Details-Dark'>reserved</span>
+          </p>
+        </div>
+        <div className='Details-Row'>
+          <p className='Details-Header'>Elapsed Time</p>
+          <p className='Details-Light'>1m</p>
+        </div>
+        <div className='Details-Row'>
+          <p className='Details-Header'>Priority</p>
+          <p className='Details-Light'>1000</p>
+        </div>
+        {showMetaData &&
+        <>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_active</p>
+            <p className='Details-Light'>true</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_arraydepend</p>
+            <p className='Details-Light'>0</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_arraydone</p>
+            <p className='Details-Light'>0</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_arrayexit</p>
+            <p className='Details-Light'>0</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_arraykilled</p>
+            <p className='Details-Light'>1</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_arrayqueued</p>
+            <p className='Details-Light'>0</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_arrayrunning</p>
+            <p className='Details-Light'>0</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_arraysuspended</p>
+            <p className='Details-Light'>0</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_depend</p>
+            <p className='Details-Light'>0</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_dgraphendtime</p>
+            <p className='Details-Light'>0</p>
+          </div>
+          <div className='Details-Row'>
+            <p className='Details-Header'>_dgraphstatus</p>
+            <p className='Details-Light'>0</p>
+          </div>
+        </>}
+        <div
+          className='Details-Show-Metadata'
+          onClick={() => {
+            setShowMetaData(!showMetaData);
+          }}
+        >
+          Show All Metadata
+        </div>
+      </div>
     </div>
   )
 }
